2b7e29f9abd32131f0104ae045beb6ff0d2b11e7
Christian Zang <christian.zang@wzw.tum.de>
2017-09-07 05:49:33 +0200

    add function to request multiple objects with regex

--- R/request.R
+++ R/request.R
@@ -19,5 +19,45 @@ request <- function(.name) {
         stop("Object is neither defined nor cached.")
       }
     }
+  } else {
+    cat(sprintf("Object %s from workspace.\n", .name))
   }
+}
+
+##' Request objects by pattern from session or cache
+##'
+##' This functions works like \link{\code{request}}, but takes a
+##'   regular expression to match against objects in workspace and
+##'   cache.
+##' @param .pattern a regular expression as string
+##' @return Nothing, invoked for side effects (requesting objects)
+##' @export
+request_all <- function(.pattern) {
+  objects <- ls()
+  in_workspace <- grep(.pattern, objects)
+  if (dir.exists("cache")) {
+    cached <- list.files("cache", pattern = "\\.rda$")
+    cached <- sub(".rda", "", cached)
+    in_cached <- grep(.pattern, cached)
+  } else {
+    in_cached <- numeric(0)
+  }
+  match_in_workspace <- length(in_workspace) > 0
+  match_in_cache <- length(in_cached) > 0
+  if (!match_in_workspace & !match_in_cache) {
+    stop("Pattern not matched in workspace or cache.")
+  }
+  if (match_in_workspace) {
+    request_workspace <- objects[in_workspace]
+  } else {
+    request_workspace <- NULL
+  }
+  if (match_in_cache) {
+    request_cache <- cached[in_cached]
+  } else {
+    request_cache <- NULL
+  }
+  request_objects <- c(request_workspace, request_cache)
+  request_objects <- request_objects[!duplicated(request_objects)]
+  invisible(lapply(request_objects, request))
 }
