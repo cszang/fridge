506617e8ad7dfb9026fea1b4157d7c5a6e4645e8
Christian Zang <zang@posteo.de>
2018-02-13 15:46:06 +0100

    request() return TRUE/FALSE
    this makes it straightforward to check for existence of cached object

--- R/request.R
+++ R/request.R
@@ -18,13 +18,15 @@ thaw <- function(str) {
 }
 
 #' Request object from session or cache
-#' 
+#'
 #' This function is a wrapper around \link{\code{thaw}}, that checks if an
 #' object is currently defined in the workspace. If not, it tries to load the
 #' object from the project's cache.
 #' @param .name String giving the name of the requested object
-#'   
-#' @return Nothing, invoked for side effects (requesting an object)
+#'
+#' @return Returns TRUE if object could be reloaded from cache or was already
+#'   present in workspace, or FALSE otherwise. Mainly invoked for side effects
+#'   (requesting an object).
 #' @export
 request <- function(.name) {
   if (!exists(.name)) {
@@ -34,12 +36,15 @@ request <- function(.name) {
       cached <- sub(".rda", "", cached)
       if (any(cached == .name)) {
         thaw(.name)
+        return(TRUE)
       } else {
-        stop("Object is neither defined nor cached.")
+        message("Object is neither defined nor cached.")
+        return(FALSE)
       }
     }
   } else {
     cat(sprintf("Object %s from workspace.\n", .name))
+    return(TRUE)
   }
 }
 
